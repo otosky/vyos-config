2ce1bc0cb0b64cd63913a6dff7f2d5e1ee4b249c
Oliver Tosky <olivertosky@gmail.com>
2023-05-29 17:28:48 -0400

    update mac addresses

--- config-parts/1-interfaces.sh
+++ config-parts/1-interfaces.sh
@@ -1,13 +1,13 @@
 #!/bin/vbash
 
 set interfaces ethernet eth0 description 'WAN'
-set interfaces ethernet eth0 hw-id ''
+set interfaces ethernet eth0 hw-id '98:fa:9b:65:f4:50'
 set interfaces ethernet eth0 mtu '1508'
 set interfaces ethernet eth0 vif 6 description 'Internet'
 set interfaces ethernet eth0 vif 6 mtu '1508'
 
 set interfaces ethernet eth1 description 'LAN'
-set interfaces ethernet eth1 hw-id ''
+set interfaces ethernet eth1 hw-id '90:e2:ba:36:e2:08'
 set interfaces ethernet eth1 address '10.66.0.1/24'
 set interfaces ethernet eth1 vif 10 address '10.66.1.1/24'
 set interfaces ethernet eth1 vif 10 description 'SERVERS'
@@ -18,6 +18,6 @@ set interfaces ethernet eth1 vif 30 description 'GUEST'
 set interfaces ethernet eth1 vif 40 address '10.66.3.1/24'
 set interfaces ethernet eth1 vif 40 description 'IOT'
 
-set interfaces ethernet eth2 hw-id 'e4:3a:6e:5f:33:9e'
-set interfaces ethernet eth3 hw-id 'e4:3a:6e:5f:33:9f'
-set interfaces ethernet eth4 hw-id 'e4:3a:6e:5f:33:a0'
+set interfaces ethernet eth2 hw-id '90:e2:ba:36:e2:09'
+set interfaces ethernet eth3 hw-id '90:e2:ba:36:e2:0a'
+set interfaces ethernet eth4 hw-id '90:e2:ba:36:e2:0b'
