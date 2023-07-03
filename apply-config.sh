766a14a8a53c8cee661db176441800cf0d004213
Oliver Tosky <olivertosky@gmail.com>
2023-07-03 01:07:22 -0400

    fix path to env file

--- apply-config.sh
+++ apply-config.sh
@@ -39,7 +39,7 @@ source /opt/vyatta/etc/functions/script-template
 # Reset the configuration
 load /opt/vyatta/etc/config.boot.default
 
-source .env
+source /config/.env
 
 # Load all config files
 for f in /config/config-parts/*.sh
