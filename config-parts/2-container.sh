3fd9fef535a823d26bc4b6eeeac4f4fce9454005
Oliver Tosky <olivertosky@gmail.com>
2023-07-03 02:38:39 -0400

    add cloudflare-ddns

--- config-parts/2-container.sh
+++ config-parts/2-container.sh
@@ -40,3 +40,16 @@ set container name haproxy-k8s-api shared-memory '0'
 set container name haproxy-k8s-api volume config source '/config/containers/haproxy/config/haproxy.cfg'
 set container name haproxy-k8s-api volume config destination '/usr/local/etc/haproxy/haproxy.cfg'
 set container name haproxy-k8s-api volume config mode 'ro'
+
+# cloudflare-ddns
+set container name cloudflare-ddns allow-host-networks
+set container name cloudflare-ddns environment CF_API_TOKEN value "${SECRET_CLOUDFLARE_DYNDNS_TOKEN}"
+set container name cloudflare-ddns environment DOMAINS value "${SECRET_CLOUDFLARE_DOMAINS}"
+set container name cloudflare-ddns environment IP6_PROVIDER value "none"
+set container name cloudflare-ddns environment TZ value 'America/New_York'
+set container name cloudflare-ddns environment PGID value "1000"
+set container name cloudflare-ddns environment PUID value "1000"
+set container name cloudflare-ddns image 'docker.io/favonia/cloudflare-ddns:1.9.4'
+set container name cloudflare-ddns memory '0'
+set container name cloudflare-ddns restart 'on-failure'
+set container name cloudflare-ddns shared-memory '0'
