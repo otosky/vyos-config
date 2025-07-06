14f094430c301a468b305d85c76d5ab119b29ecd
Oliver Tosky <olivertosky@gmail.com>
2025-07-06 17:32:41 -0400

    bridge together eth1-4

--- config-parts/1-interfaces.sh
+++ config-parts/1-interfaces.sh
@@ -7,21 +7,25 @@ set interfaces ethernet eth0 mtu '1508'
 set interfaces ethernet eth0 vif 6 description 'Internet'
 set interfaces ethernet eth0 vif 6 mtu '1508'
 
-set interfaces ethernet eth1 description 'LAN'
 set interfaces ethernet eth1 hw-id '90:e2:ba:36:e2:08'
-set interfaces ethernet eth1 address '10.66.0.1/24'
-set interfaces ethernet eth1 vif 10 address '10.66.1.1/24'
-set interfaces ethernet eth1 vif 10 description 'SERVERS'
-set interfaces ethernet eth1 vif 20 address '10.66.2.1/24'
-set interfaces ethernet eth1 vif 20 description 'TRUSTED'
-set interfaces ethernet eth1 vif 30 address '192.168.50.1/24'
-set interfaces ethernet eth1 vif 30 description 'GUEST'
-set interfaces ethernet eth1 vif 40 address '10.66.3.1/24'
-set interfaces ethernet eth1 vif 40 description 'IOT'
-
+set interfaces ethernet eth1 bridge-group br0
 set interfaces ethernet eth2 hw-id '90:e2:ba:36:e2:09'
+set interfaces ethernet eth2 bridge-group br0
 set interfaces ethernet eth3 hw-id '90:e2:ba:36:e2:0a'
+set interfaces ethernet eth3 bridge-group br0
 set interfaces ethernet eth4 hw-id '90:e2:ba:36:e2:0b'
+set interfaces ethernet eth4 bridge-group br0
+
+set interfaces bridge br0 description 'LAN'
+set interfaces bridge br0 address '10.66.0.1/24'
+set interfaces bridge br0 vif 10 address '10.66.1.1/24'
+set interfaces bridge br0 vif 10 description 'SERVERS'
+set interfaces bridge br0 vif 20 address '10.66.2.1/24'
+set interfaces bridge br0 vif 20 description 'TRUSTED'
+set interfaces bridge br0 vif 30 address '192.168.50.1/24'
+set interfaces bridge br0 vif 30 description 'GUEST'
+set interfaces bridge br0 vif 40 address '10.66.3.1/24'
+set interfaces bridge br0 vif 40 description 'IOT'
 
 set interfaces wireguard wg01 address '10.66.5.1/24'
 set interfaces wireguard wg01 description 'WIREGUARD'
